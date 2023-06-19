71373a15601413b5f0020d0582a9135f2ef36539
Jesse Hayward <jesseianhayward@gmail.com>
2023-06-18 23:39:13 -0600

    set up jquery selectors for nav
    clicking a nav item will bring up an alert with the name of the nav item

--- js/type.js
+++ js/type.js
@@ -1,3 +1,21 @@
+/* this function switches content in the terminal when a nav item is clicked */
+
+$(document).ready(function(){
+  $(".changed:nth-child(1)").on("click", function(){
+    alert("WHAT");
+  });
+  $(".changed:nth-child(2)").on("click", function(){
+    alert("WHY");
+  });
+  $(".changed:nth-child(3)").on("click", function(){
+    alert("WHO");
+  });
+  $(".changed:nth-child(4)").on("click", function(){
+    alert("GET INVOLVED");
+  });
+});
+
+
 var i = 0;
 const txt_ele = document.getElementsByClassName("gather_txt");
 var speed = 50;
