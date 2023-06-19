a05822ea9ed4e373458ecbd211090015d3bedc2b
Jesse Hayward <jesseianhayward@gmail.com>
2023-06-19 00:09:57 -0600

    hide/show finished, hamburger menu works
    need to reapply CSS to nav

--- js/type.js
+++ js/type.js
@@ -2,17 +2,32 @@
 
 $(document).ready(function(){
   $("#what").hide();
+  $("#why").hide();
+  $("#who").hide();
+  $("#get_involved").hide();
   $(".changed:nth-child(1)").on("click", function(){
     $("#what").show();
+    $("#why").hide();
+    $("#who").hide();
+    $("#get_involved").hide();
   });
   $(".changed:nth-child(2)").on("click", function(){
-    alert("WHY");
+    $("#who").show();
+    $("#what").hide();
+    $("#why").hide();
+    $("#get_involved").hide();
   });
   $(".changed:nth-child(3)").on("click", function(){
-    alert("WHO");
+    $("#why").show();
+    $("#what").hide();
+    $("#who").hide();
+    $("#get_involved").hide();
   });
   $(".changed:nth-child(4)").on("click", function(){
-    alert("GET INVOLVED");
+    $("#get_involved").show();
+    $("#what").hide();
+    $("#why").hide();
+    $("#who").hide();
   });
 });
 
